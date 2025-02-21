3756904938d38a0f68242d29a5905d16b0002a7a
Development <development@MacBookPro.fios-router.home>
2025-02-20 21:08:06 -0500

    sioanafoirgnf

--- src/app/(home)/components/Hero.tsx
+++ src/app/(home)/components/Hero.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 function Hero() {
   return (
     <section
-      className="relative h-screen bg-cover bg-center bg-no-repeat bg-fixed"
+      className="relative h-screen bg-cover bg-center bg-no-repeat bg-fixed bg-scroll-mobile"
       style={{ backgroundImage: "url('/snow-church.png')" }}
     >
       {/* Dark overlay using a custom deepBlack with adjusted opacity */}

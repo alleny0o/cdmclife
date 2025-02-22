27e6bd10c655f99eb4e07021177f97c6cc847b8a
Development <development@MacBookPro.fios-router.home>
2025-02-22 13:05:18 -0500

    fixed the html backgrounsojnfaeoiaeekanosajkeeaaeaaefaefaefaefaef

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-import type { Metadata } from "next";
 import { Open_Sans, Geist, Geist_Mono, Handlee } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header/Header";
-import Footer from "../components/footer/Footer";
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -25,22 +22,6 @@ const handlee = Handlee({
   weight: "400",
 });
 
-export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"),
-  title: {
-    default: "Christ Disciple Mission Church",
-    template: "%s | Christ Disciple Mission Church",
-  },
-  description: "Christ Disciple Mission Church – Growing in faith like a mustard seed, spreading Christ’s love through worship, discipleship, and service.",
-  openGraph: {
-    images: [
-      {
-        url: "/open-graph/open-graph.jpg",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -48,10 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full overscroll-none bg-deepBlack">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${handlee.variable} ${openSans.variable} antialiased overflow-x-hidden flex-col`}>
-        <Header />
-        <main className="relative min-h-full w-full flex-1">{children}</main>
-        <Footer />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${handlee.variable} ${openSans.variable} antialiased overflow-x-hidden flex-col`}
+      >
+        {children}
       </body>
     </html>
   );

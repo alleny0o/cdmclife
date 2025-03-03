f065db42eda9df57f9697208f23aa21c70bb1f98
Development <development@Allens-MacBook-Pro-4.local>
2025-03-03 02:26:46 -0500

    IPRJNSfeg

--- src/app/(main)/(home)/components/Hero.tsx
+++ src/app/(main)/(home)/components/Hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 function Hero() {
   return (
     <section
@@ -13,40 +11,22 @@ function Hero() {
 
       {/* Content */}
       <div className="relative h-full w-full flex items-center justify-center px-4 sm:px-6 lg:px-8">
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: {
-              opacity: 1,
-              y: 0,
-              transition: { duration: 1, staggerChildren: 0.3 },
-            },
-          }}
+        <div
           className="max-w-[90%] sm:max-w-4xl flex flex-col items-center text-center"
         >
           {/* Animated Paragraph */}
-          <motion.p
-            variants={{
-              hidden: { opacity: 0, y: 10 },
-              visible: { opacity: 1, y: 0 },
-            }}
+          <p
             className="pt-10 landscape:mb-0 max-w-2xl sm:max-w-3xl text-sm sm:text-base md:text-lg lg:text-xl text-softWhite leading-relaxed"
           >
             He gives snow like wool; he scatters the frost like ashes. He casts forth his ice like morsels; who can stand
             before his cold? He sends out his word, and melts them; he causes his wind to blow, and the waters flow.
-          </motion.p>
-          <motion.p
-            variants={{
-              hidden: { opacity: 0, y: 10 },
-              visible: { opacity: 1, y: 0 },
-            }}
+          </p>
+          <p
             className="mt-2 text-right text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-softWhite"
           >
             - Psalm 147:16-18
-          </motion.p>
-        </motion.div>
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 type HeroProps = {
   title: string;
   image: string;
@@ -21,37 +19,20 @@ function Hero(input: HeroProps) {
 
         {/* Content */}
         <div className="relative h-full w-full flex items-center justify-center px-4 sm:px-6 lg:px-8">
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={{
-              hidden: { opacity: 0, y: 20 },
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 1, staggerChildren: 0.3 },
-              },
-            }}
+          <div
             className="max-w-[90%] sm:max-w-4xl flex flex-col items-center text-center"
           >
             {/* Animated Heading */}
-            <motion.h1
-              variants={{
-                hidden: { opacity: 0, y: 10 },
-                visible: { opacity: 1, y: 0 },
-              }}
+            <h1
               className="text-4xl sm:text-5xl md:text-7xl text-white font-bold tracking-tight uppercase"
             >
               {input.title}
-            </motion.h1>
-          </motion.div>
+            </h1>
+          </div>
         </div>
 
         {/* Responsive & Animated Quote Section */}
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
+        <div
           className="absolute bottom-8 right-8 w-[425px] md:w-[590px] text-[9px] sm:text-xs md:text-sm text-white p-2 sm:p-4 hidden sm:block"
         >
           <p className="leading-relaxed text-right">
@@ -60,7 +41,7 @@ function Hero(input: HeroProps) {
             for birds, showing how something small can become very big.
           </p>
           <p className="mt-2 text-right font-semibold">- Matthew 13:31-32</p>
-        </motion.div>
+        </div>
       </section>
     </>
   );

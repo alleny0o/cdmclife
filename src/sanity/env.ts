eb3ab794d3442bf72d19a69fb143f11ba6756c6c
Development <development@MacBookPro.fios-router.home>
2025-03-23 23:28:50 -0400

    finished just need content for blank pages

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,20 +1,14 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-03'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '4lgit6r7'
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
+// function assertValue<T>(v: T | undefined, errorMessage: string): T {
+//   if (v === undefined) {
+//     throw new Error(errorMessage)
+//   }
 
-  return v
-}
+//   return v
+// }
